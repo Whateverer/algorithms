949b22e7a17099f69da810807f76583ccd1e48be
yujin <hsjes777@gmail.com>
2023-06-05 23:45:17 +0900

    DynamicProgramming

---
+++ src/DynamicProgramming.java
@@ -0,0 +1,24 @@
+public class DynamicProgramming {
+    int countPaths(boolean[][] grid, int row, int col) {
+        if(!validSquare(grid, row, col)) return 0;
+        if(isAtEnd(grid, row, col)) return 1;
+        return countPaths(grid, row + 1, col) + countPaths(grid, row, col + 1);
+    }
+
+    int countPaths(boolean[][] grid, int row, int col, int[][] paths) {
+        if(!validSquare(grid, row, col)) return 0;
+        if(isAtEnd(grid, row, col)) return 1;
+        if (paths[row][col] == 0) {
+            paths[row][col] = countPaths(grid, row + 1, col) + countPaths(grid, row, col + 1)
+        }
+        return paths[row][col];
+    }
+
+    private boolean isAtEnd(boolean[][] grid, int row, int col) {
+        return true;
+    }
+
+    boolean validSquare(boolean[][] grid, int row, int col) {
+        return true;
+    }
+}
