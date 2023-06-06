71cdfe23c8e63e4b653a498d8c83b2c76bf0fb2b
yujin <hsjes777@gmail.com>
2023-06-06 22:54:39 +0900

    오류 수정

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -9,7 +9,7 @@ public class DynamicProgramming {
         if(!validSquare(grid, row, col)) return 0;
         if(isAtEnd(grid, row, col)) return 1;
         if (paths[row][col] == 0) {
-            paths[row][col] = countPaths(grid, row + 1, col) + countPaths(grid, row, col + 1)
+            paths[row][col] = countPaths(grid, row + 1, col) + countPaths(grid, row, col + 1);
         }
         return paths[row][col];
     }
