a8b3b4236af0308f06f31c2192c606dab814a193
장유진 <jin94@exotech.kr>
2023-06-16 12:58:13 +0900

    BFS

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,17 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Vector;
+
+public class BreadthFirstSearch {
+    // 탐색을 할 때 너비를 우선으로 하여 탐색을 수행하는 탐색 알고리즘
+    // 맹목적인 탐색을 하고자 할 때
+    // Queue가 필요
+
+    int number = 7;
+    int[] visited = new int[7];
+    Vector<Integer> a = new Vector<>();
+
+    void bfs(int start) {
+    }
+
+}
